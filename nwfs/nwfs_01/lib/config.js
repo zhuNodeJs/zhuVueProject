5494f0f99a886dc4db07727727880f9d109b5691
zhuNodeJs <1749238156@qq.com>
2017-10-01 19:28:05 +0800

    commit

---
+++ nwfs/nwfs_01/lib/config.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * 新工作流 ip
+ * 
+ */
+
+//生产
+       var dp_oa_urlIp = "https://dpm.deppon.com:8881";
+ // 老的附件打开接口fssc
+       var download_file_ip ='https://dpm.deppon.com:8882';
+//测试
+
+// var dpmontal_oa_urlIp="http://192.168.17.110";
+//var download_file_ip="http://192.168.68.117:8080";
+
+// http://192.168.68.117:8080/dpm/
+// var dp_oa_urlIp_old = "http://192.168.67.47:8080";
+
+
+//var dp_oa_urlIp = "http://192.168.68.117:8080";
+//var dp_oa_urlIp = "http://10.224.197.118:8080";
+//var dp_oa_urlIp = "http://10.224.197.23:8080";
